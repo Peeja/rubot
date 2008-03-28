aa39e361fb5356d68874a61f8fd85224be387d1e
Peter Jaros <peter.a.jaros@gmail.com>
2008-03-28 11:11:43 -0400

    Implemented Aria::Robot#run.

--- lib/rubot/adapters/aria/robot.rb
+++ lib/rubot/adapters/aria/robot.rb
@@ -1,7 +1,14 @@
-class Rubot::Adapters::Aria::Robot
-  attr_reader :options
+module Rubot::Adapters::Aria
+  class Robot
+    attr_reader :options
   
-  def initialize
-    @options = {}
+    def initialize
+      @options = {}
+      @manager = RobotManager.new
+    end
+  
+    def run
+      @manager.go ""
+    end
   end
 end
--- spec/rubot/adapters/aria/robot_spec.rb
+++ spec/rubot/adapters/aria/robot_spec.rb
@@ -1,15 +1,26 @@
 require File.join(File.dirname(__FILE__), %w[.. .. .. spec_helper])
 
-# fred = Rubot::Adapters::Aria::Robot
+# fred = Rubot::Adapters::Aria::Robot.new
 # fred.options[:host] = localhost
 
-describe Rubot::Adapters::Aria::Robot do
+include Rubot::Adapters::Aria
+
+describe Robot do
   before(:each) do
-    @robot = Rubot::Adapters::Aria::Robot.new
+    @mock_manager = mock("manager")
+    RobotManager.should_receive(:new).and_return(@mock_manager)
+    @robot = Robot.new
   end
   
   it "should have options hash" do
     @robot.options[:be_awesome] = true
     @robot.options.keys.should include(:be_awesome)
   end
+  
+  it "should run the robot" do
+    @mock_manager.should_receive(:go)
+    @robot.run
+  end
+  
+  # Test args
 end
